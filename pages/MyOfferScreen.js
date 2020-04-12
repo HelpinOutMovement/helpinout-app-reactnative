191b9ca39426b0d661d0b04aa980ee45fb49be82
Sandeep Bamane <sb@Sandeeps-Mac-mini.local>
2020-04-12 16:31:16 +0530

    aligned header in my offers

--- pages/MyOfferScreen.js
+++ pages/MyOfferScreen.js
@@ -13,29 +13,29 @@ import TabWrapperComponent from './components/TabWrapperComponent';
 
 const primaryData = [
     {
-        id:412,
-        helpOption:StaticImage.FOOD,
-        name:"Tina 11111Jamna",
-        description:"has offered to help you. 14 April 2020, 10:45 am.as offered to help you. 14 April 2as offered to help you. 14 April 2",
+        id: 412,
+        helpOption: StaticImage.FOOD,
+        name: "Tina 11111Jamna",
+        description: "has offered to help you. 14 April 2020, 10:45 am.as offered to help you. 14 April 2as offered to help you. 14 April 2",
         callerInfo: "Call them on +91 543053490 Time: 6pm to 9pm.",
-        callerNumber:"+91543053490"
+        callerNumber: "+91543053490"
     },
     {
-        id:122,
-        helpOption:StaticImage.MEDICINE,
-        name:" Bhaskar Rao ",
-        description:"has offered to help you. 14 April 2020, 10:45 am",
+        id: 122,
+        helpOption: StaticImage.MEDICINE,
+        name: " Bhaskar Rao ",
+        description: "has offered to help you. 14 April 2020, 10:45 am",
         callerInfo: "Call them on +91 543053490 Time: 6pm to 9pm.",
-        callerNumber:"+91543053490"
+        callerNumber: "+91543053490"
     }
 ];
 
 const secondaryData = [
     {
-        id:121,
-        helpOption:StaticImage.AMBULANCE,
-        name:"Shyam Narayan ",
-        description:"was sent a help request on 14 April 2020, 10:45 am They will call you if they can help you.",
+        id: 121,
+        helpOption: StaticImage.AMBULANCE,
+        name: "Shyam Narayan ",
+        description: "was sent a help request on 14 April 2020, 10:45 am They will call you if they can help you.",
     },
 ];
 
@@ -45,17 +45,17 @@ function MyOfferScreen(props) {
     const [modalInfo, setModalInfo] = useState({});
 
 
-    const primaryActionHandler = (ele , actions)=> {
+    const primaryActionHandler = (ele, actions) => {
         console.log(ele.id, "$$$$", actions);
-        if(actions === AppConstant.APP_ACTION.RATE_REPORT) {
+        if (actions === AppConstant.APP_ACTION.RATE_REPORT) {
             setModalInfo({
-                type:AppConstant.APP_ACTION.RATE_REPORT,
+                type: AppConstant.APP_ACTION.RATE_REPORT,
                 ...ele
             });
             setShowModal(!showModal);
         }
     }
-    const secondaryActionHandler = (ele , actions)=> {
+    const secondaryActionHandler = (ele, actions) => {
         console.log(ele.id, "$$$$", actions);
     }
     const closePopUp = () => {
@@ -63,45 +63,63 @@ function MyOfferScreen(props) {
     }
     return (
         <Container>
-            <HeaderComponent {...props} 
-                    title="My Offers " 
-                    bgColor={colorTheme} />
+            <Header
+                style={{ backgroundColor: colorTheme }}>
+                <Left style={{
+                    flexDirection: "row", 
+                    alignItems: 'center'
+                }}>
+                    <Button
+                        transparent
+                        onPress={() => navigation.openDrawer()}>
+                        <Icon name="menu" style={{ color: "#ffffff" }} />
+                    </Button>
+
+                    <Title style={{
+                        color: "#ffffff",
+                        fontFamily: "Roboto-Medium",
+                        fontSize: 20
+                    }}>My Offers</Title>
+                </Left>
+                <Right />
+            </Header>
+
             <Content padder  >
                 <Grid>
-                <TabWrapperComponent 
-                    primaryTabTitle={translate.t(appLabelKey.offers_Received)}
-                    secondaryTabTitle={translate.t(appLabelKey.offers_Sent)}
-                    primayTabData={primaryData}
-                    primaryActionHandler={primaryActionHandler}
-                    secondaryActionHandler={secondaryActionHandler}
-                    secondaryTabData={secondaryData}
-                        />
-                   
+                    <TabWrapperComponent
+                        primaryTabTitle={translate.t(appLabelKey.offers_Received)}
+                        secondaryTabTitle={translate.t(appLabelKey.offers_Sent)}
+                        primayTabData={primaryData}
+                        primaryActionHandler={primaryActionHandler}
+                        secondaryActionHandler={secondaryActionHandler}
+                        secondaryTabData={secondaryData}
+                    />
+
                 </Grid>
 
             </Content>
-            <ModalComponent 
+            <ModalComponent
                 {...modalInfo}
-                viewName={(modalInfo && modalInfo.type)? modalInfo.type : ""}
-                showModal={showModal} 
+                viewName={(modalInfo && modalInfo.type) ? modalInfo.type : ""}
+                showModal={showModal}
                 closePopUp={closePopUp} />
 
-        <Footer>                        
-          <FooterTab>
-            <Button vertical onPress={() => props.navigation.navigate(AppConstant.APP_PAGE.DASHBOARD)}>
-              <Icon name="ios-home" style={{color:"red"}}/>
-              <Text>Home</Text>
-            </Button>
-            <Button vertical onPress={() => props.navigation.navigate(AppConstant.APP_PAGE.MY_REQUEST_SCREEN)}>
-              <Icon name="camera" />
-              <Text>My Requests</Text>
-            </Button>
-            <Button vertical active onPress={() => props.navigation.navigate(AppConstant.APP_PAGE.MY_OFFERS_SCREEN)}>
-              <Icon active name="navigate" />
-              <Text>My Offers</Text>
-            </Button>          
-          </FooterTab>
-        </Footer>                
+            <Footer>
+                <FooterTab>
+                    <Button vertical onPress={() => props.navigation.navigate(AppConstant.APP_PAGE.DASHBOARD)}>
+                        <Icon name="ios-home" style={{ color: "red" }} />
+                        <Text>Home</Text>
+                    </Button>
+                    <Button vertical onPress={() => props.navigation.navigate(AppConstant.APP_PAGE.MY_REQUEST_SCREEN)}>
+                        <Icon name="camera" />
+                        <Text>My Requests</Text>
+                    </Button>
+                    <Button vertical active onPress={() => props.navigation.navigate(AppConstant.APP_PAGE.MY_OFFERS_SCREEN)}>
+                        <Icon active name="navigate" />
+                        <Text>My Offers</Text>
+                    </Button>
+                </FooterTab>
+            </Footer>
         </Container>
     );
 }
