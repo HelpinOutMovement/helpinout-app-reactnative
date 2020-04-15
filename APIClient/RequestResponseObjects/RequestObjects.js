e8dea0ce087fffc537ac8d8edd144bc7f97728ed
VSRV Raghavan <vsrv.raghavan@gmail.com>
2020-04-15 19:02:47 +0530

    created stuffHeader as a seperate function

--- APIClient/RequestResponseObjects/RequestObjects.js
+++ APIClient/RequestResponseObjects/RequestObjects.js
@@ -5,150 +5,148 @@
 
     var APIData = {"app_id": '', "imei_no": DeviceInfo.getUniqueId(), "app_version": "0.1", 'date_time':  date  };
     class RequestObject{
-
-//Struct = (...keys) => ((...v) => keys.reduce((o, k, i) => {o[k] = v[i]; return o} , {}))
-    registerObject = (country_code, mobile_no, first_name,last_name, mobile_no_visibility, user_type, org_name, org_type, org_division)=>{
-        let data = {
-            "imei_no": DeviceInfo.getUniqueId(),
-            "os_type": DeviceInfo.getSystemName(),
-            "manufacturer_name": DeviceInfo.getBrand(),
-            "os_version": DeviceInfo.getSystemVersion(),
-            "firebase_token": "121212",
-            "app_version": "0.1",
-            "time_zone": Intl.DateTimeFormat().resolvedOptions().timeZone,
-            "country_code": country_code,
-            "mobile_no": mobile_no,
-            "first_name": first_name,
-            "last_name": last_name,
-            "mobile_no_visibility": mobile_no_visibility,
-            "user_type": user_type,
-        }    
-        
-        if(user_type != "1"){
-            data["org_name"] = org_name
-            data["org_type"] =  org_type,
-            data["org_division"] = org_division;
+        //Struct = (...keys) => ((...v) => keys.reduce((o, k, i) => {o[k] = v[i]; return o} , {}))
+        registerObject = (country_code, mobile_no, first_name,last_name, mobile_no_visibility, user_type, org_name, org_type, org_division)=>{
+            let data = {
+                "imei_no": DeviceInfo.getUniqueId(),
+                "os_type": DeviceInfo.getSystemName(),
+                "manufacturer_name": DeviceInfo.getBrand(),
+                "os_version": DeviceInfo.getSystemVersion(),
+                "firebase_token": "121212",
+                "app_version": "0.1",
+                "time_zone": Intl.DateTimeFormat().resolvedOptions().timeZone,
+                "country_code": country_code,
+                "mobile_no": mobile_no,
+                "first_name": first_name,
+                "last_name": last_name,
+                "mobile_no_visibility": mobile_no_visibility,
+                "user_type": user_type,
+            }    
+            
+            if(user_type != "1"){
+                data["org_name"] = org_name
+                data["org_type"] =  org_type,
+                data["org_division"] = org_division;
+            }
+            return this.stuffHeader(data);
         }
-        return this.addHeaders(data);
-    }
 
 
-    loginObject = (country_code, mobile_no)=>{        
-        let data = {
-            "imei_no": DeviceInfo.getUniqueId(),
-            "os_type": DeviceInfo.getSystemName(),
-            "manufacturer_name": DeviceInfo.getBrand(),
-            "os_version": DeviceInfo.getSystemVersion(),
-            "firebase_token": "121212",
-            "app_version": "0.1",
-            "time_zone": Intl.DateTimeFormat().resolvedOptions().timeZone,
-            "country_code": country_code,
-            "mobile_no": mobile_no                    
-        }        
-        return this.addHeaders(data);
-    }
-
-    locationSuggestionObject = (lat, lon, geo_accuracy, activity_type , activity_uuid)=>{
-        let data = {
-            "geo_location": lat + ","+lon ,
-            "geo_accuracy": geo_accuracy,
-            "activity_type": activity_type,
-            "activity_uuid": activity_uuid,            
+        loginObject = (country_code, mobile_no)=>{        
+            let data = {
+                "imei_no": DeviceInfo.getUniqueId(),
+                "os_type": DeviceInfo.getSystemName(),
+                "manufacturer_name": DeviceInfo.getBrand(),
+                "os_version": DeviceInfo.getSystemVersion(),
+                "firebase_token": "121212",
+                "app_version": "0.1",
+                "time_zone": Intl.DateTimeFormat().resolvedOptions().timeZone,
+                "country_code": country_code,
+                "mobile_no": mobile_no                    
+            }        
+            return this.stuffHeader(data);
         }
-        return this.addHeaders(data);
-    }
 
 
-    locationRequesterSummary = (lat, lon, geo_accuracy)=>{
-        let data = {
-            "geo_location": lat + ","+lon ,
-            "geo_accuracy": geo_accuracy                  
+        locationSuggestionObject = (lat, lon, geo_accuracy, activity_type , activity_uuid)=>{
+            let data = {
+                "geo_location": lat + ","+lon ,
+                "geo_accuracy": geo_accuracy,
+                "activity_type": activity_type,
+                "activity_uuid": activity_uuid,            
+            }
+            return this.stuffHeader(data);
         }
-        return this.addHeaders(data);
-    }
 
-    userPastActivityObject = (activity_type) =>{
-        let data = {            
-            "activity_type": activity_type                  
+
+        locationRequesterSummary = (lat, lon, geo_accuracy)=>{
+            let data = {
+                "geo_location": lat + ","+lon ,
+                "geo_accuracy": geo_accuracy                  
+            }
+            return this.stuffHeader(data);
         }
-        return this.addHeaders(data);
-    }
 
-    activityAdd = () =>{
-        let data = {
-
+        userPastActivityObject = (activity_type) =>{
+            let data = {            
+                "activity_type": activity_type                  
+            }
+            return this.stuffHeader(data);
         }
-        return this.addHeaders(data);
-    }
 
-    activitySuggestions = (activity_type, activity_uuid, offerer, requester) =>{
-        let data = {
-            "activity_type": activity_type,              
-            "activity_uuid": activity_uuid,
-            "offerer": offerer,
-            "requester": requester,
+        activityAdd = () =>{
+            let data = {
+
+            }
+            return this.stuffHeader(data);
         }
-        return this.addHeaders(data);
-    }
 
-
-    activityMapping = (activity_type, activity_uuid, offerer, requester) =>{
-        let data = {
-            "activity_type": activity_type, 
-            "activity_uuid": activity_uuid, 
-            "offerer": offerer, 
-            "requester": requester
+        activitySuggestions = (activity_type, activity_uuid, offerer, requester) =>{
+            let data = {
+                "activity_type": activity_type,              
+                "activity_uuid": activity_uuid,
+                "offerer": offerer,
+                "requester": requester,
+            }
+            return this.stuffHeader(data);
         }
-        return this.addHeaders(data);
-    }
 
-    activityDelete = (activity_uuid, activity_type) =>{
-        let data = {
-            "activity_uuid": activity_uuid, 
-            "activity_type": activity_type
+
+        activityMapping = (activity_type, activity_uuid, offerer, requester) =>{
+            let data = {
+                "activity_type": activity_type, 
+                "activity_uuid": activity_uuid, 
+                "offerer": offerer, 
+                "requester": requester
+            }
+            return this.stuffHeader(data);
         }
-        return this.addHeaders(data);
-    }
 
-
-    mappingDelete = (activity_uuid, activity_type, mapping_id) =>{
-        let data = {
-            "activity_uuid": activity_uuid, 
-            "activity_type": activity_type, 
-            "mapping_id": mapping_id
+        activityDelete = (activity_uuid, activity_type) =>{
+            let data = {
+                "activity_uuid": activity_uuid, 
+                "activity_type": activity_type
+            }
+            return this.stuffHeader(data);
         }
-        return this.addHeaders(data);        
-    }
 
 
-    mappingRating = (activity_uuid, mapping_id, rating, recommend_other) =>{
-        let data = {
-            "activity_uuid":activity_uuid, 
-            "mapping_id":mapping_id, 
-            "rating":rating, 
-            "recommend_other":recommend_other
+        mappingDelete = (activity_uuid, activity_type, mapping_id) =>{
+            let data = {
+                "activity_uuid": activity_uuid, 
+                "activity_type": activity_type, 
+                "mapping_id": mapping_id
+            }
+            return this.stuffHeader(data);        
         }
-        return this.addHeaders(data);
-    }
 
 
-    mappingCall = (activity_uuid, mapping_id) =>{        
-        let data = {
-            "activity_uuid":activity_uuid, 
-            "mapping_id":mapping_id
+        mappingRating = (activity_uuid, mapping_id, rating, recommend_other) =>{
+            let data = {
+                "activity_uuid":activity_uuid, 
+                "mapping_id":mapping_id, 
+                "rating":rating, 
+                "recommend_other":recommend_other
+            }
+            return this.stuffHeader(data);
         }
-        return this.addHeaders(data);        
-    }
 
 
+        mappingCall = (activity_uuid, mapping_id) =>{        
+            let data = {
+                "activity_uuid":activity_uuid, 
+                "mapping_id":mapping_id
+            }
+            return this.stuffHeader(data);        
+        }
 
 
-    addHeaders = (data) =>{
-        let reqData = APIData;
-        reqData["data"] = data; 
-        console.log(reqData)   
-        return reqData;
-    }
+        stuffHeader = (data) =>{
+            let reqData = APIData;
+            reqData["data"] = data; 
+            console.log(reqData)   
+            return reqData;
+        }
 
 
 }
