af823544210819ca3f80889e1b79039efeccd33d
Sandeep Bamane <sb@Sandeeps-Mac-mini.local>
2020-04-18 03:32:03 +0530

    changes for login screen removed extra curly braces

--- pages/LoginScreen.js
+++ pages/LoginScreen.js
@@ -174,7 +174,7 @@ export default class LoginScreen extends React.Component {
                 <View style={{alignItems: "center",  width:"100%"}} >                                        
                     <View style={{ alignItems: "center" ,  marginVertical: 0, width:"98%"}} >
                         <TouchableOpacity style={{borderRadius: 9, marginVertical: 30,alignItems: "center",backgroundColor: "#4F5065",height: 56,width: "92%",shadowOpacity: 0.9,shadowOffset: { height: 3 },shadowColor: '#2328321F',}} onPress={() =>{this.handleSendCode()}}>
-                            <Text style={{textAlign: "center",fontFamily: "Roboto-Medium",fontSize: 20,lineHeight: 56,color: "#FFFFFF"}}>{translate.t("Login_Sign_Up")}}</Text>
+                            <Text style={{textAlign: "center",fontFamily: "Roboto-Medium",fontSize: 20,lineHeight: 56,color: "#FFFFFF"}}>{translate.t("Login_Sign_Up")}</Text>
                         </TouchableOpacity>                    
                     </View>                
                     <Text style={{
